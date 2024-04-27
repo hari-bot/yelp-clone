bec96f42f6d463a5803ec8b526f900346a8a9e20
01XASSASSIN <hariharanuv7@gmail.com>
2024-04-27 22:27:20 +0530

    getRestaurant,updaterestaurant on get route

--- server/controllers/restaurant.js
+++ server/controllers/restaurant.js
@@ -11,6 +11,23 @@ export const getRestaurants = async (req, res) => {
   }
 };
 
+export const getRestaurant = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await pool.query("SELECT * FROM restaurants WHERE id=$1", [
+      id,
+    ]);
+    if (response.rowCount === 1) {
+      const restaurants = response.rows[0];
+      res.status(200).json(restaurants);
+    } else {
+      res.status(404).json({ message: "Restaurant not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const addRestaurant = async (req, res) => {
   try {
     const { name, location, pricerange } = req.body;
@@ -39,6 +56,28 @@ export const deleteRestaurant = async (req, res) => {
 
     if (deletedRestaurant.rowCount === 1) {
       res.json({ message: "Restaurant deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Restaurant not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateRestaurant = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, location, pricerange } = req.body;
+    const updatedRestaurant = await pool.query(
+      "UPDATE restaurants SET name=$1, location=$2, pricerange=$3 WHERE id=$4 RETURNING *",
+      [name, location, pricerange, id]
+    );
+
+    if (updatedRestaurant.rowCount === 1) {
+      const updatedData = updatedRestaurant.rows[0];
+      res
+        .status(200)
+        .json({ message: "Restaurant updated successfully", updatedData });
     } else {
       res.status(404).json({ message: "Restaurant not found" });
     }
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
   getRestaurants,
+  getRestaurant,
   addRestaurant,
+  updateRestaurant,
   deleteRestaurant,
 } from "../controllers/restaurant.js";
 
@@ -9,9 +11,11 @@ const router = Router();
 
 //read
 router.get("/", getRestaurants);
+router.get("/:id", getRestaurant);
 
 //update
 router.post("/", addRestaurant);
 router.delete("/:id", deleteRestaurant);
+router.patch("/:id", updateRestaurant);
 
 export default router;
